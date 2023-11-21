8dd461c6eef2d22fd3507efe3ac6bb47c75d200b
jeffercs96 <jeffercs96@gmail.com>
2023-11-20 21:56:27 -0500

    Add Singleton project to solution

---
+++ Singleton/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine(Singleton.Singleton.Instance.mensaje);
+Singleton.Singleton.Instance.mensaje = "Hola mundo desde el singleton";
+Console.WriteLine(Singleton.Singleton.Instance.mensaje);
---
+++ Singleton/Singleton.cs
@@ -0,0 +1,23 @@
+﻿namespace Singleton;
+
+public class Singleton
+{
+    private static Singleton _instance = null;
+    public string mensaje = "";
+    protected Singleton()
+    {
+        mensaje = "Hola mundo";
+    }
+    public static Singleton Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = new Singleton();
+            }
+            return _instance;
+        }
+    }
+
+}
